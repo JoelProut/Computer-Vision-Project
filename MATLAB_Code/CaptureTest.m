2838958c9192900ebf6aa69c16a205ba8766e966
JoelProut <43254182+JoelProut@users.noreply.github.com>
2019-03-31 18:42:53 -0700

    findProduct working with webcam

--- MATLAB_Code/CaptureTest.m
+++ MATLAB_Code/CaptureTest.m
@@ -1,3 +1,4 @@
+clc; clear all; close all;
 %Capture Test
 webcamlist
 %Type in name from list
@@ -5,4 +6,9 @@ camName = 'USB2.0 PC CAMERA';
 %Create a webcam object
 cam = webcam(camName);
 %view camera live feed
-preview(cam)
+%preview(cam)
+
+while true
+    img = snapshot(cam);
+    findProduct("FrostedFlakes",img);
+end
--- MATLAB_Code/findProduct.m
+++ MATLAB_Code/findProduct.m
@@ -2,9 +2,8 @@
 % images.
 % For the sake of testing I'm only having two options, LuckyCharms
 % CaptainCrunch, or Frootloops
-function Ifinal = findProduct(product)
+function Ifinal = findProduct(product,scene)
 %test product
-product = "FrostedFlakes";
 if(product == "CaptainCrunch")
     template = imread('CaptainCrunchTemplate.jpg');
 elseif (product == "LuckyCharms")
@@ -19,23 +18,23 @@ else
 end
 
 template = rgb2gray(template);
-figure, imshow(template),title("template");
-scene = rgb2gray(imread("testImage.jpg"));
-figure, imshow(scene), title("scene");
+% figure, imshow(template),title("template");
+scene = rgb2gray(scene);
+% figure, imshow(scene), title("scene");
 
 %% Feature Detection
 tempPoints = detectSURFFeatures(template);
 scenePoints = detectSURFFeatures(scene);
-%Strongest template features
-figure, imshow(template), title('100 Strongest Feature Points')
-hold on;
-plot(tempPoints.selectStrongest(100));
-hold off;
-%Strongest scene features
-figure, imshow(scene), title('500 Strongest Scene Points')
-hold on;
-plot(scenePoints.selectStrongest(500));
-hold off;
+% %Strongest template features
+% figure, imshow(template), title('100 Strongest Feature Points')
+% hold on;
+% plot(tempPoints.selectStrongest(100));
+% hold off;
+% %Strongest scene features
+% figure, imshow(scene), title('500 Strongest Scene Points')
+% hold on;
+% plot(scenePoints.selectStrongest(500));
+% hold off;
 
 %% Feature Descriptors
 tempFeatures = extractFeatures(template, tempPoints);
@@ -43,30 +42,28 @@ sceneFeatures = extractFeatures(scene, scenePoints);
 featurePairs = matchFeatures(tempFeatures, sceneFeatures);
 matchedTempPoints = tempPoints(featurePairs(:,1),:);
 matchedScenePoints = scenePoints(featurePairs(:,2),:);
-figure, showMatchedFeatures(template, scene, matchedTempPoints, matchedScenePoints, 'montage');
-title('Matched Points with Outliers');
+% figure, showMatchedFeatures(template, scene, matchedTempPoints, matchedScenePoints, 'montage');
+% title('Matched Points with Outliers');
 %% Locate object and remove outlying matches
-[tform, inlierTempPoints, inlierScenePoints] = estimateGeometricTransform(matchedTempPoints, matchedScenePoints, 'affine', 'MaxNumTrials',2000,'MaxDistance',16);
-figure, showMatchedFeatures(template, scene, inlierTempPoints, inlierScenePoints, 'montage');
- %% Box image
-% Edge detection
-S = strel('disk',15);
-I2 = imopen(template,S);  
-E = edge(I2, 'canny',.2,1);
-%Find bb
-[L,N] = bwlabel(E);
-props = regionprops(L,'all');
-bb = props.BoundingBox; %[x,y,w,h]
-%convert bb to line coordinates
-boundingBox = [bb(1), bb(2); bb(1) + bb(3), bb(2); bb(1) + bb(3), bb(2) + bb(4); bb(1), bb(2) + bb(4); bb(1), bb(2)]; %[x1,y1; x2, y1; x2, y2; x1,y1; x1,y1]
-newbb = transformPointsForward(tform, boundingBox); % Use the geometric transform to project the bounding box into the scene image
+if size(matchedTempPoints,1) >= 4 && size(matchedTempPoints,1) >= 4
+    [tform, inlierTempPoints, inlierScenePoints] = estimateGeometricTransform(matchedTempPoints, matchedScenePoints, 'projective','MaxDistance',20);
 
-imshow(scene);
-disp(newbb(:,1));
-hold on;
-line(newbb(:,1), newbb(:,2), 'Color', 'r');
-title('Detected Box');
- 
+% figure, showMatchedFeatures(template, scene, inlierTempPoints, inlierScenePoints, 'montage');
+%% Box image using a transformation matrix
+        boxPolygon = [1, 1;... % top-left
+        size(template, 2), 1;... % top-right
+        size(template, 2), size(template, 1);... % bottom-right
+        1, size(template, 1);... % bottom-left
+        1, 1]; % top-left again to close the polygon
+	
+        newBoxPolygon = transformPointsForward(tform, boxPolygon);
+        %Draw a yellow bounding box around the detected image
+        imshow(scene),hold on;
+        line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'r');
+        hold off;
+else
+    return;
+end
 
 
 end
