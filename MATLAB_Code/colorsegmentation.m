c6e3878e08dc29c340e8a34f7866d9e83074d60c
patrickmcarlos <patrickmcarlos@hotmail.com>
2019-03-31 19:12:27 -0700

    updated

--- MATLAB_Code/colorsegmentation.m
+++ MATLAB_Code/colorsegmentation.m
@@ -1,4 +1,4 @@
-function result = colorsegmentation(I)
+function [fruit, meanHue] = colorsegmentation(I)
 %figure, imshow(I), title("Apple");
 
 lab_img = rgb2lab(I);
@@ -7,6 +7,7 @@ ab = lab_img(:,:,2:3);
 ab = im2single(ab);
 nColors = 2;
 
+%% Cluster the Image
 % repeat the clustering 3 times to avoid local minima
 pixel_labels = imsegkmeans(ab, nColors, 'NumAttempts', 3);
 %imshow(pixel_labels, []), title("Image Labeled by Cluster Index");
@@ -16,21 +17,49 @@ mask1 = pixel_labels==2;
 cluster = I .* uint8(mask1);
 bin = im2bw(cluster, 0.25);
 bin = bwareafilt(bin, 1);
+
+%% Convert to HSV
 hsv = rgb2hsv(cluster);
+[h s v] = imsplit(hsv);
 
-figure, subplot(2,3,1), imshow(cluster), title("Objects in Cluster");
-subplot(2,3,2), imshow(bin), title("binary");
-subplot(2,3,3), imshow(hsv), title("hsv");
+
+%% Display HSV & Binary Images
+%figure, subplot(2,3,1), imshow(cluster), title("Objects in Cluster");
+%subplot(2,3,2), imshow(bin), title("binary");
+%subplot(2,3,3), imshow(h), title("h");
+
 % Iterate through the cluster image, wherever the binary image is 1
-[col, row] = size(I);
-
-for x = 1 : col
-    for y = 1 : row
-        if bin(x, y) == 1
-            
+count = 0;
+sum = 0;
+for x = 1 : size(I, 1)
+    for y = 1 : size(I, 2)
+        if bin(x,y) == 1
+            sum = sum + hsv(x,y,1);
+            count = count + 1;
         end
     end
 end
+
+meanHue = sum/count;
+fruit = "N/A";
+
+% Print Result
+if meanHue < 0.14
+    %subplot(2,3,4), imshow(I), title("Orange (" + meanHue + ")");
+    fruit = "Orange";
+elseif meanHue > 0.14 && meanHue < 0.22
+    %subplot(2,3,4), imshow(I), title("Banana (" + meanHue + ")");
+    fruit = "Banana";
+elseif meanHue > 0.4 && meanHue < 0.8
+    %subplot(2,3,4), imshow(I), title("Apple (" + meanHue + ")");
+    fruit = "Apple";
+else
+    %subplot(2,3,4), imshow(I), title("Unable to Determine (" + meanHue + ")");
+    fruit = "N/A";
+end
+
+return
+
 
 
 
--- MATLAB_Code/main.m
+++ MATLAB_Code/main.m
@@ -1,9 +1,79 @@
-orange = imread("orange.jpeg");
-apple = imread("apple.jpg");
-apple2 = imread("apple2.jpg");
-banana = imread("banana.jpg");
+%% Webcam
+webcamlist
 
-colorsegmentation(orange);
-colorsegmentation(apple);
-colorsegmentation(apple2);
-colorsegmentation(banana);
+camName = 'Integrated Webcam';
+
+% Create a webcam object
+cam = webcam(camName);
+
+%view camera live feed
+preview(cam);
+
+closePreview(cam);
+
+recentAssessment = "N/A";
+count = 0;
+
+while true
+   pause(0.10)
+   % Acquire a single image.
+   rgbImage = snapshot(cam);
+    
+   [fruit, meanHue] = colorsegmentation(rgbImage);
+  
+   imshow(rgbImage)
+   
+   if count == 7
+       % DO SOMETHING
+       title(fruit + " (" + meanHue + ")");
+       if recentAssessment ~= fruit
+           count = 0;
+           title("Calculating");
+       end
+   elseif recentAssessment == fruit
+       count = count + 1;
+       title("Calculating");
+   else
+       count = 0;
+       recentAssessment = fruit;
+       title("Calculating");
+   end
+end
+
+clear('cam');
+%% TEST LINES
+% test1 = imread("orange.jpeg");
+% test2 = imread("orange2.jpg");
+% test3 = imread("orange3.jpg");
+% test4 = imread("orange4.jpg");
+% test5 = imread("apple.jpg");
+% test6 = imread("apple2.jpg");
+% test7 = imread("apple3.jpg");
+% test8 = imread("apple4.jpg");
+% test9 = imread("apple5.jpg");
+% test10 = imread("banana.jpg");
+% test11 = imread("lettuce.jpg");
+% 
+% [fruit1, meanHue1] = colorsegmentation(test1);
+% [fruit2, meanHue2] = colorsegmentation(test2);
+% [fruit3, meanHue3] = colorsegmentation(test3);
+% [fruit4, meanHue4] = colorsegmentation(test4);
+% [fruit5, meanHue5] = colorsegmentation(test5);
+% [fruit6, meanHue6] = colorsegmentation(test6);
+% [fruit7, meanHue7] = colorsegmentation(test7);
+% [fruit8, meanHue8] = colorsegmentation(test8);
+% [fruit9, meanHue9] = colorsegmentation(test9);
+% [fruit10, meanHue10] = colorsegmentation(test10);
+% [fruit11, meanHue11] = colorsegmentation(test11);
+% 
+% figure, subplot(3,4,1), imshow(test1), title([fruit1] + [meanHue1]);
+% subplot(3,4,2), imshow(test2), title([fruit2] + [meanHue2]);
+% subplot(3,4,3), imshow(test3), title([fruit3] + [meanHue3]);
+% subplot(3,4,4), imshow(test4), title([fruit4] + [meanHue4]);
+% subplot(3,4,5), imshow(test5), title([fruit5] + [meanHue5]);
+% subplot(3,4,6), imshow(test6), title([fruit6] + [meanHue6]);
+% subplot(3,4,7), imshow(test7), title([fruit7] + [meanHue7]);
+% subplot(3,4,8), imshow(test8), title([fruit8] + [meanHue8]);
+% subplot(3,4,9), imshow(test9), title([fruit9] + [meanHue9]);
+% subplot(3,4,10), imshow(test10), title([fruit10] + [meanHue10]);
+% subplot(3,4,11), imshow(test11), title([fruit11] + [meanHue11]);
